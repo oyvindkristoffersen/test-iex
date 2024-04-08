ea7a995356627b1c2e189e421328ee4e8cbf8c6e
oyvindkristoffersen <165886059+oyvindkristoffersen@users.noreply.github.com>
2024-04-08 17:32:24 +0200

    Create YourScript.ps1

---
+++ YourScript.ps1
@@ -0,0 +1,8 @@
+# YourScript.ps1
+# This PowerShell script simply writes a sentence to the screen
+
+# The sentence to write
+$Sentence = "Hello, this is my script running from the web!"
+
+# Write the sentence to the host/console
+Write-Host $Sentence

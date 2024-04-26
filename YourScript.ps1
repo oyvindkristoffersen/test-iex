571eeb3a1bb939d3d823b66c39dece2f031d68dd
oyvindkristoffersen <165886059+oyvindkristoffersen@users.noreply.github.com>
2024-04-26 16:29:24 +0200

    Update YourScript.ps1

--- YourScript.ps1
+++ YourScript.ps1
@@ -4,9 +4,12 @@
 # The sentence to write
 $Sentence = "Hello, this is my script running from the web!"
 $OutputPath = "C:\Run\Test\Output.txt"
+$OutputPath2 = "C:\Run\Test\Output.txt"
+@Sentence2 = ls
 # Write the sentence to the host/console
 Write-Host $Sentence
+Write-Host $Sentence2
 Add-Content -Value $Sentence -Path $OutputPath
-
+Add-Content -Value $Sentence2 -Path $OutputPath2
 
 
